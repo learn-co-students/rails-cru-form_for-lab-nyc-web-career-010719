b791e6683aeafa6c53a0fff7dd7d41ea971a9df8
cvolpe12 <christopher.volpe12@gmail.com>
2019-02-04 16:38:22 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Artist.create(name: "Drake", bio: "Views from the 6")
+a2 = Artist.create(name: "Justin Timberlake", bio: "Boy Band to Solo Artist")
+a3 = Artist.create(name: "Chris Stapleton", bio: "Country boy")
+a4 = Artist.create(name: "Queen", bio: "the best there was")
+a5 = Artist.create(name: "Calvin Harris", bio: "best beat maker")
+
+g1 = Genre.create(name: "hip-hop")
+g2 = Genre.create(name: "rock")
+g3 = Genre.create(name: "pop")
+g4 = Genre.create(name: "country")
+g5 = Genre.create(name: "techno")
+
+s1 = Song.create(name: "Over", artist_id: a1.id, genre_id: g1.id)
+s2 = Song.create(name: "Suit & Tie", artist_id: a2.id, genre_id: g3.id)
+s3 = Song.create(name: "Tennessee Whiskey", artist_id: a3.id, genre_id: g4.id)
+s4 = Song.create(name: "Bohemian Rhapsody", artist_id: a4.id, genre_id: g2.id)
+s5 = Song.create(name: "Another One Bites the Dust", artist_id: a4.id, genre_id: g3.id)
+s6 = Song.create(name: "Electricity", artist_id: a5.id, genre_id: g5.id)
+s7 = Song.create(name: "Headlines", artist_id: a1.id, genre_id: g1.id)
+s8 = Song.create(name: "Mirrors", artist_id: a1.id, genre_id: g3.id)
